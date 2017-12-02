6d2ae39b6a3bda9f84d274d1c6ddb5c7b84dcfe1
ANKIT KUMAR <ankit247shbm@gmail.com>
2017-12-02 08:16:42 +0530

    Update Permutation.cpp

--- Permutation.cpp
+++ Permutation.cpp
@@ -20,6 +20,16 @@ void permutation(uint64_t *EncryptedNumber, uint64_t key1[])
 
 int main()
 {
- 
+  uint64_t EncryptNumber[16], permuterd_key[16];
+  uint64_t key1[16], key2[16];
+  int i;
+  for(int i = 0; i < 16; i++)                     /* For sake of understanding EncryptNumber is taken as a user input. */
+  {
+   cin >> EncryptNumber[i];
+  }
+  for(int i = 0; i < 16; i++)                      /* But key1 Scheme is taken as a user input. */
+  {
+    cin >> key1[i];
+  }
   return 0;
 }
