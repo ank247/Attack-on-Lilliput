5e1423d3f7018544b4fc6527aaf2986332d0507d
ANKIT KUMAR <ankit247shbm@gmail.com>
2017-12-02 08:05:28 +0530

    Update LILLIPUT_Scheme.cpp

--- LILLIPUT_Scheme.cpp
+++ LILLIPUT_Scheme.cpp
@@ -82,19 +82,19 @@ void Encrypted_Number(uint64_t *EncryptedNumber, uint64_t *b, int l, int m, uint
     EncryptedNumber[mid + j] = J[mid + j];
   }
   j++;
-  return Encrypted_Number(EncryptedNumber, b, l, m, key, S_Box);       /* Recursion function call. */
+  return Encrypted_Number(EncryptedNumber, b, l, m, key, S_Box);    /* Recursion function call. */
 }
 
 void permutation(uint64_t *EncryptedNumber, uint64_t key1[])
 {
-  uint64_t key2[16], permuted_number[16];                             /* Final output is permuted_number Array. */
+  uint64_t key2[16], permuted_number[16];                           /* Final output is permuted_number Array. */
   for(int i = 0; i < 16; i++)
   {
-    key2[key[i]] = EncryptNumber[i];
+    key2[key[i]] = EncryptNumber[i];                                /* Stored key1 Array will instruct permutation. */
   }
   for(int i = 0; i < 16; i++)
   {
-     EncryptNumber[i] = key2[i] ;
+     EncryptNumber[i] = key2[i] ;                                  /* For next round EncryptNumber Array will serve as an initial Array. */
   }
   for(int i = 0; i < 16; i++)
   {
