6999765df2e4a70d3138cf17730fb1361cf88a92
ANKIT KUMAR <ankit247shbm@gmail.com>
2017-12-02 08:12:14 +0530

    Update Permutation.cpp

--- Permutation.cpp
+++ Permutation.cpp
@@ -1,12 +1,25 @@
 #include<iostream>
-using namespace std
+using namespace std;
 
-void permutation(uint64_t * EncryptedNumber)
+void permutation(uint64_t *EncryptedNumber, uint64_t key1[])
 {
- 
+  uint64_t key2[16], permuted_number[16];                           /* Final output is permuted_number Array. */
+  for(int i = 0; i < 16; i++)
+  {
+    key2[key[i]] = EncryptNumber[i];                                /* Stored key1 Array will instruct permutation. */
+  }
+  for(int i = 0; i < 16; i++)
+  {
+     EncryptNumber[i] = key2[i] ;                                  /* For next round EncryptNumber Array will serve as an initial Array. */
+  }
+  for(int i = 0; i < 16; i++)
+  {
+     permuted_number[i] = key2[16-i] ;
+  }
 }
 
 int main()
 {
+ 
   return 0;
 }
