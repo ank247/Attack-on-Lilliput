0767699102128d59c4fb1f9cc03afa1c92e058a2
ANKIT KUMAR <ankit247shbm@gmail.com>
2017-12-03 00:24:04 +0530

    Update Variance_Method.cpp

--- Variance_Method.cpp
+++ Variance_Method.cpp
@@ -6,13 +6,14 @@ int main()
   uint64_t *EncryptNumber ;              /* EncryptNumber is the array we get before permutation. */
   uint64_t key[16], SBox[16];            /* key and SBox is an array of length 16 consisting [1,0] arbitrary bits, an user input.  */
   int i;
-  for(i = 0; i < 16; i++)
+  for(i = 0; i < 16; i++)              
   {
     cin >> key[i]; 
   }
-   for(i = 0; i < 16; i++)
+  for(i = 0; i < 16; i++)              /* For now as a convenience taken as an user input. */
   {
     cin >> SBox[i]; 
   }
+  
   return 0;
 }
