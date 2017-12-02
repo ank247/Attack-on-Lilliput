b0aa3fb205086639fab1434f57d852e511f9aa3a
ANKIT KUMAR <ankit247shbm@gmail.com>
2017-12-03 00:19:52 +0530

    Update Variance_Method.cpp

--- Variance_Method.cpp
+++ Variance_Method.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main()
 {
+  uint64_t *EncryptNumber ;              /* EncryptNumber is the array we get before permutation. */
+  uint64_t key[16], SBox[16];            /* key and SBox is an array of length 16 consisting [1,0] arbitrary bits, a user input.  */
   
- return 0;
+  return 0;
 }
